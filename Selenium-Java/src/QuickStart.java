8cdece670d0e4aa1ad4ea50a409b06b4f3b4525d
sridharbandi <sridhar.bandi.ece@gmail.com>
2018-07-11 22:34:25 +0100

    Added Java Selenium Test

--- Selenium-Java/src/QuickStart.java
+++ Selenium-Java/src/QuickStart.java
@@ -1,3 +1,19 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class QuickStart {
 
+    public static void main(String[] args) throws InterruptedException {
+
+        WebDriver driver = new ChromeDriver();
+        driver.get("http://www.google.com/xhtml");
+        Thread.sleep(5000);
+        WebElement searchBox = driver.findElement(By.name("q"));
+        searchBox.sendKeys("ChromeDriver");
+        searchBox.submit();
+        Thread.sleep(5000);
+        driver.quit();
+    }
 }
