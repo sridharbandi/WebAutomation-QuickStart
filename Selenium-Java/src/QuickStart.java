fc34d44cad6bb5ff0270193e3a5f8e3aa0a26428
sridharbandi <sridhar.bandi.ece@gmail.com>
2018-07-11 22:45:22 +0100

    Added Driver Path with Driversource property

--- Selenium-Java/src/QuickStart.java
+++ Selenium-Java/src/QuickStart.java
@@ -2,11 +2,13 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeDriverService;
 
 public class QuickStart {
 
-    public static void main(String[] args) throws InterruptedException {
-        System.setProperty("webdriver.chrome.driver","./drivers/chromedriver");
+    public static void main(String[] args){
+        System.setProperty(ChromeDriverService.CHROME_DRIVER_EXE_PROPERTY,"./drivers/chromedriver");
+        //System.setProperty("webdriver.chrome.driver","./drivers/chromedriver");
         WebDriver driver = new ChromeDriver();
         driver.get("http://www.google.com");
         WebElement searchBox = driver.findElement(By.id("lst-ib"));
